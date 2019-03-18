39f11b7a88f64592a97044dcfefe8d1906a123c7
ben@redpointegroup.com <ben@redpointegroup.com>
2019-03-18 09:11:42 -0500

    Initial commit

---
+++ Visual Studio 2017/Backup Files/RedPointeTimeCapture/Original-Jan-16-2019-0826AM.Admin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedPointeTimeCapture.Models
+{
+    public class Admin
+    {
+        public int ID { get; set; }
+        public string AdminName { get; set; }
+
+
+    }
+}
---
+++ Visual Studio 2017/Backup Files/RedPointeTimeCapture/Recovered-Jan-16-2019-0826AM.Admin.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedPointeTimeCapture.Models
+{
+    public class Admin
+    {
+        public int ID { get; set; }
+     
+
+
+    }
+
+    Admin AdminName = new Admin;
+
+}
